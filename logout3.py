e540d90ca3f35a00f3fa5e67e5ce2d0022fe7f9a
Asim Irfan <rajputasim721@gmail.com>
2025-02-24 16:11:16 +0500

    testc2 and logout3.py has been added

---
+++ logout3.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+gecko_path = "C:\\Users\\Al Habib Trade\\Desktop\\Selenium\\geckodriver-v0.35.0-win64\\geckodriver.exe"
+service = Service(gecko_path)
+driver = webdriver.Firefox(service=service)
+
+try:
+    driver.get("https://testc2.fwt-logi.com/AdminArea/index.php")
+    wait=WebDriverWait(driver,10)
+    username_field = wait.until(expected_conditions.presence_of_element_located((By.XPATH, "//input[@placeholder='Enter user name']")))
+    password_field = wait.until(expected_conditions.presence_of_element_located((By.XPATH, "//input[@placeholder='Enter password']")))
+    username_field.send_keys("sysadmin")
+    password_field.send_keys("12345678")
+    login_button = wait.until(expected_conditions.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Sign In')]")))
+    login_button.click()
+    wait.until(expected_conditions.url_changes("https://testc2.fwt-logi.com/AdminArea/index.php"))
+    print("Login successful! Current URL:", driver.current_url)
+
+    welcome_button = wait.until(expected_conditions.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Welcome Ali Zahid' )]")))
+    welcome_button.click()
+    logout_button = wait.until(expected_conditions.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Logout')]")))
+    logout_button.click()
+    wait.until(expected_conditions.url_contains("index.php"))
+    print("Logout successful! Redirected to Login Page.")
+
+except Exception as e:
+    print("Error occurred:", e)
