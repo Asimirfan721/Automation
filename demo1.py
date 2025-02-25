2ef25b65db7dbd45dd0d6e385fda127042e62ddd
Asim Irfan <rajputasim721@gmail.com>
2025-02-25 14:53:35 +0500

    password_field issue resolved

--- demo1.py
+++ demo1.py
@@ -2,24 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
+# Set up geckodriver
 gecko_path = "C:\\Users\\Al Habib Trade\\Desktop\\Selenium\\geckodriver-v0.35.0-win64\\geckodriver.exe"
 service = Service(gecko_path)
 driver = webdriver.Firefox(service=service)
 
-try:
+try: 
+    # Open the website
     driver.get("https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F")
     wait = WebDriverWait(driver, 10)
-    username_field = wait.until(expected_conditions.presence_of_element_located((By.XPATH, "//input[@placeholder='Please enter your email']")))
-    password_field = wait.until(expected_conditions.presence_of_element_located((By.XPATH, "//input[@placeholder='Password']")))
-    username_field.send_keys("admin@yourstore.com")
-    password_field.send_keys("admin")
-    login_button = wait.until(expected_conditions.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Sign In')]")))
+    username_field = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Please enter your email']" )))
+    password_field = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Password']" )))
+    username_field.send_keys("sysadmin")
+    password_field.send_keys("12345678")
+    login_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Log in')]")))
     login_button.click()
-    wait.until(expected_conditions.url_changes("https://admin-demo.nopcommerce.com/admin/"))
+    wait.until(EC.url_changes("https://admin-demo.nopcommerce.com/admin/"))
+
     print("Login successful! Current URL:", driver.current_url)
-    
 except Exception as e:
     print("Error occurred:", e)
- 
